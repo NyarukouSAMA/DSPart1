6c3ab6fc9a719a49eb10d38a7d272d2130bec44c
NyarukouSAMA <afro.detray@gmail.com>
2021-04-04 23:17:58 +0300

    123

--- DocString.py
+++ DocString.py
@@ -1,14 +1,2 @@
-import pandas as pd
-
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-
-# Create a dataframe from a dict
-import pandas as pd
-from numpy.random import randint
-
-df = pd.DataFrame(columns=['lib', 'qty1', 'qty2'])
-for i in range(5):
-    df.loc[i] = ['name' + str(i)] + list(randint(10, size=2))
-
-print(df)
+import sys
+print (sys.path)
